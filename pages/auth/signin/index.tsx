13967c38bdbc7c0f3b6e252f6c61c6a8f7f02e40
Fail <afail1234ahmet@gmail.com>
2025-10-18 22:52:14 +0300

    fix: auth input type

--- pages/auth/signin/index.tsx
+++ pages/auth/signin/index.tsx
@@ -1,78 +1,80 @@
-import { BackButton } from "@/components/back-button";
-import { useForm } from "effector-forms";
-import { useTheme } from "styled-components/native";
-import { $form } from "./model";
-import * as S from "./styled";
+import { BackButton } from '@/components/back-button'
+import { useForm } from 'effector-forms'
+import { useTheme } from 'styled-components/native'
+import { $form } from './model'
+import * as S from './styled'
 
-import { Typography } from "@/components";
-import { Field } from "@/components/field";
-import { authModel } from "@/entities/auth";
-import { useUnit } from "effector-react";
-import { Stack, useRouter } from "expo-router";
-import { ScrollView, StyleSheet } from "react-native";
+import { Typography } from '@/components'
+import { Field } from '@/components/field'
+import { authModel } from '@/entities/auth'
+import { useUnit } from 'effector-react'
+import { Stack, useRouter } from 'expo-router'
+import { ScrollView, StyleSheet } from 'react-native'
 
 export const SignInPageUI = () => {
-  const theme = useTheme();
-  const router = useRouter();
+	const theme = useTheme()
+	const router = useRouter()
 
-  const isAuth = useUnit(authModel.$isAuth);
-  const isPending = useUnit(authModel.signInFx.pending);
-  const form = useForm($form);
+	const isAuth = useUnit(authModel.$isAuth)
+	const isPending = useUnit(authModel.signInFx.pending)
+	const form = useForm($form)
 
-  const toggleAuthType = useUnit(authModel.toggleAuthModalType);
+	const toggleAuthType = useUnit(authModel.toggleAuthModalType)
 
-  const styles = StyleSheet.create({
-    title: {
-      marginTop: !isAuth ? 40 : 0,
-      marginBottom: 20,
-    },
-    centerText: {
-      marginInline: "auto",
-    },
-  });
+	const styles = StyleSheet.create({
+		title: {
+			marginTop: !isAuth ? 40 : 0,
+			marginBottom: 20,
+		},
+		centerText: {
+			marginInline: 'auto',
+		},
+	})
 
-  return (
-    <ScrollView overScrollMode="always">
-      <S.SignInPageStyled>
-        <Stack.Screen
-          options={{
-            title: "sign-in",
-          }}
-        />
-        {isAuth && <BackButton onPress={() => router.push("/")} />}
-        <S.SignInPageTitle style={styles.title}>Логин</S.SignInPageTitle>
-        <Field
-          error={form.fields.email.errors?.[0]?.errorText}
-          placeholder="Email"
-          onChangeText={form.fields.email.onChange}
-        />
-        <Field
-          error={form.fields.password.errors?.[0]?.errorText}
-          placeholder="Password"
-          onChangeText={form.fields.password.onChange}
-        />
-        <S.SignInButton
-          $rippleColor={theme.background}
-          onPress={() => form.submit()}
-        >
-          <Typography $variant="semibold" $color={theme.background}>
-            Войти
-          </Typography>
-        </S.SignInButton>
-        <S.SignInLink
-          $rippleColor={theme.background}
-          onPress={toggleAuthType}
-          disabled={isPending}
-        >
-          <Typography
-            $variant="semibold"
-            $color={theme.foreground}
-            style={styles.centerText}
-          >
-            Зарегистрироваться
-          </Typography>
-        </S.SignInLink>
-      </S.SignInPageStyled>
-    </ScrollView>
-  );
-};
+	return (
+		<ScrollView overScrollMode='always'>
+			<S.SignInPageStyled>
+				<Stack.Screen
+					options={{
+						title: 'sign-in',
+					}}
+				/>
+				{isAuth && <BackButton onPress={() => router.push('/')} />}
+				<S.SignInPageTitle style={styles.title}>Логин</S.SignInPageTitle>
+				<Field
+					error={form.fields.email.errors?.[0]?.errorText}
+					placeholder='Email'
+					keyboardType='email-address'
+					onChangeText={form.fields.email.onChange}
+				/>
+				<Field
+					error={form.fields.password.errors?.[0]?.errorText}
+					placeholder='Password'
+					secureTextEntry
+					onChangeText={form.fields.password.onChange}
+				/>
+				<S.SignInButton
+					$rippleColor={theme.background}
+					onPress={() => form.submit()}
+				>
+					<Typography $variant='semibold' $color={theme.background}>
+						Войти
+					</Typography>
+				</S.SignInButton>
+				<S.SignInLink
+					$rippleColor={theme.background}
+					onPress={toggleAuthType}
+					disabled={isPending}
+				>
+					<Typography
+						$variant='semibold'
+						$color={theme.foreground}
+						style={styles.centerText}
+					>
+						Зарегистрироваться
+					</Typography>
+				</S.SignInLink>
+			</S.SignInPageStyled>
+		</ScrollView>
+	)
+}
--- pages/auth/signup/index.tsx
+++ pages/auth/signup/index.tsx
@@ -1,102 +1,105 @@
-import { BackButton } from "@/components/back-button";
-import { useForm } from "effector-forms";
-import { useTheme } from "styled-components/native";
-import { $form } from "./model";
-import * as S from "./styled";
+import { BackButton } from '@/components/back-button'
+import { useForm } from 'effector-forms'
+import { useTheme } from 'styled-components/native'
+import { $form } from './model'
+import * as S from './styled'
 
-import { Typography } from "@/components";
-import { Field } from "@/components/field";
-import { Switch } from "@/components/switch";
-import { authModel } from "@/entities/auth";
-import { useUnit } from "effector-react";
-import { Stack, router } from "expo-router";
-import { useCallback } from "react";
-import { ScrollView, StyleSheet } from "react-native";
+import { Typography } from '@/components'
+import { Field } from '@/components/field'
+import { Switch } from '@/components/switch'
+import { authModel } from '@/entities/auth'
+import { useUnit } from 'effector-react'
+import { Stack, router } from 'expo-router'
+import { useCallback } from 'react'
+import { ScrollView, StyleSheet } from 'react-native'
 
 export const SignUpPageUI = () => {
-  const theme = useTheme();
+	const theme = useTheme()
 
-  const isAuth = useUnit(authModel.$isAuth);
-  const isPending = useUnit(authModel.signUpFx.pending);
-  const form = useForm($form);
+	const isAuth = useUnit(authModel.$isAuth)
+	const isPending = useUnit(authModel.signUpFx.pending)
+	const form = useForm($form)
 
-  const toggleAuthType = useUnit(authModel.toggleAuthModalType);
+	const toggleAuthType = useUnit(authModel.toggleAuthModalType)
 
-  const styles = StyleSheet.create({
-    title: {
-      marginTop: !isAuth ? 40 : 0,
-      marginBottom: 20,
-    },
-    centerText: {
-      marginInline: "auto",
-    },
-    right: {
-      marginLeft: "auto",
-    },
-  });
+	const styles = StyleSheet.create({
+		title: {
+			marginTop: !isAuth ? 40 : 0,
+			marginBottom: 20,
+		},
+		centerText: {
+			marginInline: 'auto',
+		},
+		right: {
+			marginLeft: 'auto',
+		},
+	})
 
-  const handlePress = useCallback(() => {
-    router.replace("/");
-  }, []);
+	const handlePress = useCallback(() => {
+		router.replace('/')
+	}, [])
 
-  return (
-    <ScrollView overScrollMode="always">
-      <S.SignUpPageStyled>
-        <Stack.Screen
-          options={{
-            title: "sign-up",
-          }}
-        />
-        {isAuth && <BackButton onPress={handlePress} />}
-        <S.SignUpPageTitle style={styles.title}>Регистрация</S.SignUpPageTitle>
-        <Switch
-          label="Аккаунт сообщества"
-          value={form.fields.is_group.value}
-          onValueChange={form.fields.is_group.onChange}
-          style={styles.right}
-        />
-        <Field
-          error={form.fields.name.errors?.[0]?.errorText}
-          placeholder="Ваше имя"
-          onChangeText={form.fields.name.onChange}
-        />
-        <Field
-          error={form.fields.email.errors?.[0]?.errorText}
-          placeholder="Email"
-          onChangeText={form.fields.email.onChange}
-        />
-        <Field
-          error={form.fields.password.errors?.[0]?.errorText}
-          placeholder="Пароль"
-          onChangeText={form.fields.password.onChange}
-        />
-        <Field
-          error={form.fields.confirmPassword.errors?.[0]?.errorText}
-          placeholder="Подтвердите пароль"
-          onChangeText={form.fields.confirmPassword.onChange}
-        />
-        <S.SignUpButton
-          $rippleColor={theme.background}
-          onPress={() => form.submit()}
-        >
-          <Typography $variant="semibold" $color={theme.background}>
-            Зарегаться
-          </Typography>
-        </S.SignUpButton>
-        <S.SignInLink
-          $rippleColor={theme.background}
-          onPress={toggleAuthType}
-          disabled={isPending}
-        >
-          <Typography
-            $variant="semibold"
-            $color={theme.foreground}
-            style={styles.centerText}
-          >
-            Войти
-          </Typography>
-        </S.SignInLink>
-      </S.SignUpPageStyled>
-    </ScrollView>
-  );
-};
+	return (
+		<ScrollView overScrollMode='always'>
+			<S.SignUpPageStyled>
+				<Stack.Screen
+					options={{
+						title: 'sign-up',
+					}}
+				/>
+				{isAuth && <BackButton onPress={handlePress} />}
+				<S.SignUpPageTitle style={styles.title}>Регистрация</S.SignUpPageTitle>
+				<Switch
+					label='Аккаунт сообщества'
+					value={form.fields.is_group.value}
+					onValueChange={form.fields.is_group.onChange}
+					style={styles.right}
+				/>
+				<Field
+					error={form.fields.name.errors?.[0]?.errorText}
+					placeholder='Ваше имя'
+					onChangeText={form.fields.name.onChange}
+				/>
+				<Field
+					error={form.fields.email.errors?.[0]?.errorText}
+					placeholder='Email'
+					keyboardType='email-address'
+					onChangeText={form.fields.email.onChange}
+				/>
+				<Field
+					error={form.fields.password.errors?.[0]?.errorText}
+					placeholder='Пароль'
+					secureTextEntry
+					onChangeText={form.fields.password.onChange}
+				/>
+				<Field
+					error={form.fields.confirmPassword.errors?.[0]?.errorText}
+					placeholder='Подтвердите пароль'
+					secureTextEntry
+					onChangeText={form.fields.confirmPassword.onChange}
+				/>
+				<S.SignUpButton
+					$rippleColor={theme.background}
+					onPress={() => form.submit()}
+				>
+					<Typography $variant='semibold' $color={theme.background}>
+						Зарегаться
+					</Typography>
+				</S.SignUpButton>
+				<S.SignInLink
+					$rippleColor={theme.background}
+					onPress={toggleAuthType}
+					disabled={isPending}
+				>
+					<Typography
+						$variant='semibold'
+						$color={theme.foreground}
+						style={styles.centerText}
+					>
+						Войти
+					</Typography>
+				</S.SignInLink>
+			</S.SignUpPageStyled>
+		</ScrollView>
+	)
+}
